aae3db2dd61ee79add8f7c10e74d72a8563b1ac0
Joshpowerfit <nwankwojoshua100@gmail.com>
2023-08-02 01:17:06 +0000

    Task

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python3
 """A Basic Flask app with internationalization support.
 """
-from flask_babel import Babel
 from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
+app = Flask(__name__)
 
+# Config class to define available languages and set default locale and timezone
 class Config:
-    """Represents a Flask Babel configuration.
-    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
-app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
+
+# Instantiate the Babel object
 babel = Babel(app)
 
 
+# get_locale function to determine the best match for supported languages
 @babel.localeselector
-def get_locale() -> str:
-    """Retrieves the locale for a web page.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+def get_locale():
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('3-index.html')
+def index():
+    return render_template(
+        '4-index.html',
+        home_title=_("Welcome to Holberton"),
+        home_header=_("Hello world!")
+    )
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
+
